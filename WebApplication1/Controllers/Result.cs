2d4d75df557bdc714ad6557b7aa0eb34e4b7ec11
suvarnba <suvarnaba21@gmail.com>
2020-09-04 13:21:09 +0530

    Result file changed

--- WebApplication1/Controllers/Result.cs
+++ WebApplication1/Controllers/Result.cs
@@ -13,7 +13,7 @@ namespace WebApplication1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<Result> _logger;
